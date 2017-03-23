5b142778cc8c06a984a6b0b15e861983a8adcfa6
Nam Luu <nam.luuduc@gmail.com>
2017-03-23 22:40:25 +0700

    update user login

--- controllers/user.controller.php
+++ controllers/user.controller.php
@@ -80,6 +80,31 @@ class UserController extends Controller
 
     public function login()
     {
+        if ($_POST && isset($_POST['username']) && isset($_POST['password'])) {
+            $username = $this->model->escape($_POST['username']);
+            $password = $this->model->escape($_POST['password']);
+
+            $user = $this->model->getBy('email', $username);
+            $hash = md5(Config::get('salt').$password);
+            if ($user && $user['is_active'] && $hash === $user['password']) {
+                Session::set('username', $user['username']);
+                Session::set('role', 'user');
+                Router::redirect(getUrl('user/account'));
+            } else {
+                Session::setMessage('error', 'Wrong account');
+            }
+        }
+        View::renderView();
+    }
+
+    public function logout()
+    {
+        Session::destroy();
+        Router::redirect(getUrl());
+    }
+
+    public function account()
+    {
         View::renderView();
     }
 }
